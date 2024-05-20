68ca8243b21ce24f6a4d2b33da8d823c07431cba
sairaj chikyala <sairaj.chikyala@gmail.com>
2024-05-20 18:01:53 +0530

    changeing the order and removing the old content in backend

--- backend.sh
+++ backend.sh
@@ -1,6 +1,8 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
+rm -rf/app
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
@@ -8,7 +10,7 @@ cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
-cp backend.service /etc/systemd/system/backend.service
+
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
